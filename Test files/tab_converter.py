5587d1be65a65a2147fe460aec2a89e6035ba69a
sankalpmukim <sankalpmukim@gmail.com>
2021-04-05 20:01:07 +0530

    Test files framework

---
+++ Test files/tab_converter.py
@@ -0,0 +1,7 @@
+inp = open('inputTimetable.txt', newline='')
+readData = inp.read()
+print(readData)
+opString = repr(readData)[1:-1]
+print(opString)
+op = open('outputTimetable.txt', 'w', newline='')
+op.write(opString)
